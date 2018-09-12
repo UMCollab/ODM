efdcbfee537356ef11599200e27709f767eacde7
Paul Arthur <paul.arthur@flowerysong.com>
2018-09-12 05:32:09 +0000

    Cosmetic: clean up redundant and unused imports

--- odm/googledriveclient.py
+++ odm/googledriveclient.py
@@ -6,7 +6,6 @@
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
-import json
 import os
 import random
 import requests
--- odm/onedriveclient.py
+++ odm/onedriveclient.py
@@ -7,11 +7,9 @@ from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
 import base64
-import json
 import os
 import random
 import re
-import requests
 import sys
 import time
 
