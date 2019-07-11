56f4134424705962d50bd854ee20e16f290537f0
Paul Arthur <paul.arthur@flowerysong.com>
2019-07-10 22:24:44 -0400

    Add `odm list split`
    This was initially implemented and tested as an external script, but
    this is a fine place for it to live.

--- odm/libexec/odm_list.py
+++ odm/libexec/odm_list.py
@@ -13,6 +13,8 @@ import os
 import sys
 import time
 
+from copy import copy
+
 import dateutil.parser
 
 import odm.cli
@@ -21,7 +23,7 @@ import odm.ms365
 
 def main():
     odm.cli.CLI.writer_wrap(sys)
-    cli = odm.cli.CLI(['--filetree', '--upload-user', '--upload-group', '--upload-path', '--domain-map', '--limit', '--exclude', '--diff', 'file', 'action'])
+    cli = odm.cli.CLI(['--filetree', '--upload-user', '--upload-group', '--upload-path', '--domain-map', '--length', '--split-prefix', '--limit', '--exclude', '--diff', 'file', 'action'])
     client = cli.client
 
     ts_start = datetime.datetime.now()
@@ -315,6 +317,55 @@ def main():
                     cli.logger.info(u'Removing %s', relfpath)
                     fpath = '/'.join([root, fname])
                     os.unlink(fpath)
+
+    elif cli.args.action == 'split':
+        if cli.args.length:
+            length = int(cli.args.length)
+        else:
+            length = 500
+
+        split_prefix = cli.args.split_prefix or cli.args.file
+
+        count = 0
+        split = 0
+
+        for item_id in metadata['items']:
+	    item = metadata['items'][item_id]
+	    if 'file' not in item:
+		continue
+
+	    item['odm_split'] = [split]
+
+	    while 'id' in item['parentReference']:
+		item =  metadata['items'][item['parentReference']['id']]
+		item['odm_split'] = set(item.get('odm_split', [])).union([split])
+
+	    count += 1
+	    if count >= length:
+		split += 1
+		count = 0
+
+	for i in range(0, split + 1):
+	    fname = '{}{:0{align}d}.json'.format(
+		split_prefix,
+		i,
+		align = len(str(split))
+	    )
+            cli.logger.debug(u'Saving list %d to %s', i, fname)
+
+	    output = {
+		'items': {}
+	    }
+	    for item_id in metadata['items']:
+		item = metadata['items'][item_id]
+		if 'odm_split' not in item or i in item['odm_split']:
+		    output['items'][item_id] = copy(item)
+		    output['items'][item_id].pop('odm_split', None)
+
+	    with open(fname, 'w') as f:
+		json.dump(output, f, indent = 2)
+
+        cli.logger.info(u'Split %s into %d chunks of %d', cli.args.file, split + 1, length)
 
     else:
         cli.logger.critical(u'Unsupported action %s', cli.args.action)
