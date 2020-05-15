f7e1e206a86e89023112e35a513425b9ceffc12c
Paul Arthur <paul.arthur@flowerysong.com>
2020-05-15 14:26:25 -0400

    pipenv: restore primacy of setuptools
    No more duplication of dependencies in Pipfile or
    `pipenv run pip install -e .` hack.
    
    Also move static setuptools data into setup.cfg and add a simple
    pyproject.toml.

--- env-setup.sh
+++ env-setup.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-hacking_dir=$(readlink -fn $(dirname "$BASH_SOURCE"))
-cd $hacking_dir
-pipenv run pip install -e .
 alias bm='pipenv run bm'
 alias gdm='pipenv run gdm'
 alias odm='pipenv run odm'
